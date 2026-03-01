31fb974b58dd8e1c64dd278f1fd74cc45314af2c
haaatchi27 <hatchi_for_accoun2@yahoo.co.jp>
2026-03-01 14:24:55 +0900

    fix DnD for android rev2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,13 +8,10 @@ import './index.css'
 
 // Import and initialize mobile-drag-drop polyfill
 import { polyfill } from "mobile-drag-drop";
-import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour-drag-image-translate-override";
 
 polyfill({
     // drag and drop items on hold (150ms) to avoid conflict with scroll
-    holdToDrag: 150,
-    // custom scroll behavior during drag
-    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+    holdToDrag: 150
 });
 
 // Optionally prevent browser from scrolling while dragging
